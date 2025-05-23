bbaebf93444daea0833a767727d5b073ed09cbaa
BhavyaTarigopula12 <163883306+BhavyaTarigopula12@users.noreply.github.com>
2025-05-23 23:35:16 +0530

    Update work.jsx

--- work.jsx
+++ work.jsx
@@ -26,17 +26,7 @@ function Work() {
               <img className="object-cover object-center h-auto max-w-full rounded-lg" src="https://github.com/BhavyaTarigopula12/Lift-website-/blob/98ad3339d0f075cf1881ad88f82d26e8cbfb5759/IMG-20250522-WA0003.jpg&fit=crop&w=800&q=80" alt="gallery-photo" />
             </div>
           </div>
-          <div className="grid gap-4">
-            <div>
-              <img className="object-cover object-center h-auto max-w-full rounded-lg" src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80" alt="gallery-photo" />
-            </div>
-            <div>
-              <img className="object-cover object-center h-auto max-w-full rounded-lg " src="https://docs.material-tailwind.com/img/team-3.jpg" alt="gallery-photo" />
-            </div>
-            <div>
-              <img className="object-cover object-center h-auto max-w-full rounded-lg" src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="gallery-photo" />
-            </div>
-          </div>
+          
           </div>
         </div>
       </div>
